f09a313f89f4ae61949cb8efe2df3cfa800d71c4
kkjumin <kkjumin@gmail.com>
2020-06-24 13:53:08 +0900

    start

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "no-undef": "off",
+    "no-shadow": "off",
+    "no-param-reassign": ["error", { props: false }],
+    "linebreak-style": ["error", "windows"],
+    "object-curly-newline": "off",
+    "global-require": "off",
+    "func-names": "off"
+  },
   parserOptions: {
     parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
 };
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  semi: true,
+  printWidth: 150,
+  singleQuote: true
+};
